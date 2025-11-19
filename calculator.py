34cf8e72600c72499bd15238930a80596382373b
Leon Grigoruk <38705951+egdwashwa@users.noreply.github.com>
2025-11-18 22:16:12 -0500

    Rename subtraction and logarithm functions

--- calculator.py
+++ calculator.py
@@ -18,7 +18,7 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def sub(a,b):
+def subtract(a,b):
     return a - b
 
 def mul(a,b):
@@ -29,7 +29,7 @@ def div(a,b):
         raise ZeroDivisionError
     return b/a
 
-def log(a,b):
+def logarithm(a,b):
     if a <= 0 or a==1:
         raise ValueError
     if b <= 0:
